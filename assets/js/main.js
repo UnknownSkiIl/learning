ca9c0304bb28e30fbe7befbaef045ea232836859
Sergey <hukl856@gmail.com>
2019-02-21 19:07:09 +0200

    Solved problem #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-let money,
-    time;
-
-money = prompt("Ваш бюджет на месяц?");
-time = prompt("Введите дату в формате YYYY-MM-DD");
+let money= +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
     appMoney : money,
@@ -13,15 +10,65 @@ let appData = {
     savings : false
 }
 
+// let i = 0;
+// while (i < 2) {
+//     i++;
+//     let answerOne = prompt('Введите обязательную статью расходов в этом месяце');
+//     answerTwo = prompt('Во сколько обойдется?');
 
-answerOne = prompt('Введите обязательную статью расходов в этом месяце');
-answerTwo = prompt('Во сколько обойдется?');
-
-expenses: {
-    answerOne : answerTwo;
+//     if (typeof (answerOne) === 'string' && answerOne != '' && answerTwo != '' && answerOne.length < 51 && answerTwo.length < 51) {
+//         console.log('done');
+//         appData.expenses[answerOne] = answerTwo;
+//     } else {
+//         alert('Введите правильные данные');
+//         i--;
+//     }
+// }
+let i = 0;
+do {
+    
 }
 
-alert("Ваш бюджет на день - " + (money - answerTwo) / 30);
+while (i < 2) {
+    i++
+    let answerOne = prompt('Введите обязательную статью расходов в этом месяце');
+    answerTwo = prompt('Во сколько обойдется?');
+
+    if (typeof (answerOne) === 'string' && answerOne != '' && answerTwo != '' && answerOne.length < 51 && answerTwo.length < 51) {
+        console.log('done');
+        appData.expenses[answerOne] = answerTwo;
+    } else {
+        alert('Введите правильные данные');
+        i--;
+    }
+}
+
+// for (let i = 0; i < 2; i++) {
+//     let answerOne = prompt('Введите обязательную статью расходов в этом месяце');
+//         answerTwo = prompt('Во сколько обойдется?');
+    
+//     if (typeof (answerOne) === 'string' && answerOne != '' && answerTwo != '' && answerOne.length < 51 && answerTwo.length < 51) {
+//         console.log('done');
+//         appData.expenses[answerOne] = answerTwo;
+//     } else {
+//         alert('Введите правильные данные');
+//         i--;
+//     }
+// };
+
+appData.moneyPerDay = appData.appMoney / 30;
+
 console.log(appData);
+
+if(appData.moneyPerDay < 100) {
+    console.log('Минимал даун')
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('deafuld chel')
+} else if (appData.moneyPerDay >= 2000){
+    console.log('tupa da da ya!')
+} else {
+    console.log('Error')
+}
+
 
 
